f84cef86f636d30165273c4811830e39a1c55ba2
asaxena2019 <anushkasaxena2019@gmail.com>
2020-10-24 00:14:47 -0400

    changed server again

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const http = require('http')
 const express = require('express')
-
 const app = express()
+
 app.use(express.static('public'))
 
-app.set('port', process.env.PORT || '3000')
+//app.set('port', '3000')
 
 const server = http.createServer(app)
+
 server.on('listening', () => {
- console.log('Listening on port 3000')
+ console.log('Listening on ports')
 })
 
 // Web sockets
